c2f4ba3f2ebb4f3e8095fd5c852b3c4dc7f732f3
Edi Santoso <edicyber@gmail.com>
2015-10-15 09:48:25 +0700

    Try Implementing Guzzle and consume Api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,16 +25,55 @@ Route::get('give-me-token', function () {
 
 Route::get('cetak-pdf', 'CetakPdfController@index');
 
-Route::post('guzzle', function () {
+Route::get('guzzle', function () {
+
+//    $client = new \GuzzleHttp\Client();
+//    $body = ['akun' => 'Akun Saya', 'kode_rekening' => '02'];
+//    $response = $client->post('http://appsim.dev/api/v1/akun', [
+//        'form_params' => $body,
+//        'headers'     => [
+//            'X-Requested-With' => 'XMLHttpRequest'
+//        ]
+//    ]);
+
+//    echo $response->getBody();
 
     $client = new \GuzzleHttp\Client();
-    $body = ['akun' => 'Akun Saya', 'kode_rekening' => '02'];
-    $response = $client->post('http://appsim.dev/api/v1/akun', [
-        'form_params' => $body,
-        'headers'     => [
-            'X-Requested-With' => 'XMLHttpRequest'
-        ]
-    ]);
+    $response = $client->get('http://demoapi.ubig.co.id/api/price.asmx/Iklan_Search?AccessKey_App=561cecc9096f2043c05b7736&Ngr=id&i_sts=555e8f32b05097100cb1741e&kywd=avanza&seo_kat=otomotif-dan-sepeda&seo_skat=mobil&seo_jns=&hrg_start=&hrg_end=&kond=&i_prop=&i_kot=&i_smbr=&t_psg_start=&t_psg_end=&Sort=&Limit=10&Page=1&Atribut=&Bhs=idn');
 
-    echo $response->getBody();
+    $result = XML2JSON($response->getBody());
+
+    $data = json_decode($result);
+
+    return response()->json($data->pagging->Total);
+
 });
+
+function XML2JSON($xml)
+{
+
+    function normalizeSimpleXML($obj, &$result)
+    {
+        $data = $obj;
+        if (is_object($data)) {
+            $data = get_object_vars($data);
+        }
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $res = null;
+                normalizeSimpleXML($value, $res);
+                if (($key == '@attributes') && ($key)) {
+                    $result = $res;
+                } else {
+                    $result[$key] = $res;
+                }
+            }
+        } else {
+            $result = $data;
+        }
+    }
+
+    normalizeSimpleXML(simplexml_load_string($xml), $result);
+
+    return json_encode($result);
+}
